4e7eb35762c97a4838a554258964410e14c5586e
MaiIwamoto <gng9134@gmail.com>
2024-06-30 01:10:52 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,14 @@ class UsersController < ApplicationController
   end
   
   def index
-    @user = User.find(params[:id])
-    @books = @user.books.page(params[:page])
+    @books = Book.page(params[:page])
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 end
